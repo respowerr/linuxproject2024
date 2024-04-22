73aed9aa49fa1692b4804cdd0305a3d5599cf243
Respowerr <respowerryt@gmail.com>
2024-04-22 21:35:57 +0200

    Second change

--- menu.sh
+++ menu.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+echo "==== PROJET LINUX ===="
+PS3="Votre choix : "
 
-PS3="Sélectionnez un script à exécuter : "
-
-options=("Créer des utilisateurs" "Afficher les informations des utilisateurs" "Contrôler les fichiers SUID et SGID" "Quitter")
+options=("Créer des utilisateurs" "Afficher les informations des utilisateurs" "Contrôler les fichiers SUID et SGID" "Quitter l'application")
 
 select opt in "${options[@]}"
 do
     case $REPLY in
-        1) ./create_users.sh ;;
-        2) ./show_users.sh ;;
-        3) ./check_suid_sgid.sh ;;
+        1) 
+            if [ -f "fichier_source.txt" ]; then
+                ./create.sh fichier_source.txt
+            else
+                echo "Le fichier source 'fichier_source.txt' n'existe pas."
+            fi
+            ;;
+        2) ./show.sh ;;
+        3) ./control.sh ;;
         4) echo "Au revoir !"; exit ;;
         *) echo "Option invalide";;
     esac
